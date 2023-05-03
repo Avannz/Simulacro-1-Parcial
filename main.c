a532466dbbcd7a88c397a25f83a3e0aa6c8d814e
JoaquinTaborda <130505200+Avannz@users.noreply.github.com>
2023-05-02 21:47:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -20,16 +20,38 @@ int main()
     int arreglo[dim];
     int arreglo2[dim];
     int validos;
+    #include <stdio.h>
+#include <stdlib.h>
+#include "pila.h"
+
+void cargarPila (Pila *pila);
+int pasarArreglo (int arreglo[], int dim, Pila *pila);
+void mostrarArreglo (int arreglo[], int validos);
+int buscarMayor(int arreglo[], int validos);
+int buscarMenorPila (Pila *pila);
+void quitarMenor (Pila *pila, int menor;);
+void buscarMenor2 (Pila *pila);
+int cargarArreglo(int a[], int dim);
+float promArr(int arreglo[], int validos);
+
+int main()
+{
+    Pila pila1;
+    inicpila(&pila1);
+    int dim = 20;
+    int arreglo[dim];
+    int arreglo2[dim];
+    int validos;
     int mayor;
     int menor;
-    float promedio = 0;
+    float promedio;
 
-//    cargarPila(&pila1);
+    cargarPila(&pila1);
 //    mostrar(&pila1);
+
+    validos = pasarArreglo(arreglo, dim, &pila1);
+    mostrarArreglo(arreglo, validos);
 //
-//    validos = pasarArreglo(arreglo, dim, &pila1);
-//    mostrarArreglo(arreglo, validos);
-////
 ////    mayor = buscarMayor(arreglo, validos);
 ////    printf("\n El mayor numero del arreglo es: | %d |", mayor);
 ////
@@ -39,9 +61,8 @@ int main()
 ////    buscarMenor2(&pila1);
 ////    mostrar(&pila1);
 
-    validos = cargarArreglo(arreglo2, dim);
-    promedio = promArr(arreglo2, validos);
-    printf("%f", promedio);
+    promedio = promArr(arreglo, validos);
+    printf("\n%.2f", promedio);
 }
 
 
@@ -174,43 +195,25 @@ void buscarMenor2 (Pila *pila){
 
 }
 
-int promArr(int arreglo[], int validos){
+float promArr(int arreglo[], int validos){
 
     int i = 0;
     int suma = 0;
-    float promedio = 0;
+    int suma2 = 0;
+    float promedio;
 
     while(i<validos){
 
-        suma = suma + arreglo[i];
-
+        suma = arreglo[i];
+        suma2 = suma2 + suma;
         i++;
 
     }
 
-    promedio = suma / validos;
+    promedio = suma2 / validos;
 
     return promedio;
 
-}
-
-int cargarArreglo(int a[], int dim){
-
-    int i = 0;
-    int cant;
-
-    printf("Cuantas notas va a ingresar?");
-    fflush(stdin);
-    scanf("%i", &cant);
-
-    for(i=0;i<cant;i++){
-
-        printf("Ingresa un valor");
-        fflush(stdin);
-        scanf("%d", a[i]);
-    }
-
-    return i;
 }
 
 
